6408ae490e50b85eba51ee62a3393e086b7d68b7
Steven Leyva <Sleyva1991@outlook.com>
2019-10-02 18:19:31 -0400

    started and finished the Storyboard added bear and user file and constructed the GigsController

---
+++ Gigs/Gigs/Controller/GigsController.swift
@@ -0,0 +1,9 @@
+//
+//  GigsController.swift
+//  Gigs
+//
+//  Created by Steven Leyva on 10/2/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ Gigs/Gigs/Model/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  Gigs
+//
+//  Created by Steven Leyva on 10/2/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
